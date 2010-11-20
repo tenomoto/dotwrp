0fcefc6fbf6016d5686cbb94821ffe1e4063a9ff
Takeshi Enomoto <takeshi@enomosphere.net>
2010-11-20 13:42:41 +0900

    * First commit 	new file:   dotwrp.f90

---
+++ dotwrp.f90
@@ -0,0 +1,40 @@
+!g95 -fno-underscoring dotwrp.f90 -c
+double complex function zdotc_(n, zx, incx, zy, incy)
+double complex zx(*), zy(*), z
+integer n, incx, incy
+
+call cblas_zdotc_sub(%val(n), zx, %val(incx), zy, %val(incy), z)
+
+zdotc_ = z
+return
+end
+
+double complex function zdotu_(n, zx, incx, zy, incy)
+double complex zx(*), zy(*), z
+integer n, incx, incy
+
+call cblas_zdotu_sub(%val(n), zx, %val(incx), zy, %val(incy), z)
+
+zdotu_ = z
+return
+end
+
+complex function cdotc_(n, cx, incx, cy, incy)
+complex cx(*), cy(*), c
+integer n, incx, incy
+
+call cblas_cdotc_sub(%val(n), cx, %val(incx), cy, %val(incy), c)
+
+cdotc_ = c
+return
+end
+
+complex function cdotu_(n, cx, incx, cy, incy)
+complex cx(*), cy(*), c
+integer n, incx, incy
+
+call cblas_cdotu_sub(%val(n), cx, %val(incx), cy, %val(incy), c)
+
+cdotu_ = c
+return
+end
